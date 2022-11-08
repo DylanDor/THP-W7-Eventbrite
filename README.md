1962dcb13b45dd973cb0f7d244e28f3f167664da
Dylan D <dore.dylan31@gmail.com>
2022-11-08 20:15:07 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+w7eventbrite.fly.dev
---
+++ app/models/user.rb
@@ -0,0 +1,21 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+    after_create :welcome_send
+    
+    has_many :attendances, dependent: :destroy
+    has_many :events, through: :attendances
+    has_many :administrated_events, foreign_key: 'admin_id', class_name: 'Event'
+
+    validates :email,
+        presence: true,
+        uniqueness: true
+    validates :first_name, presence: true
+    validates :last_name, presence: true
+
+    def welcome_send
+    UserMailer.welcome_email(self).deliver_now
+    end
+end
